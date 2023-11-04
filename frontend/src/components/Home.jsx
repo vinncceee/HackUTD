436fc8d1410c57fb923cad3fee8edceba9415565
vinncceee <vbanks2k@gmail.com>
2023-11-04 18:36:49 -0500

    added color pal

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,23 @@
 export default function Home() {
     return (
-        <div>
-            <div className="header 1">
-                <div className="Info">
-                    <h1>Header</h1>
-                    <image></image>
+        <div className="flex justify-center items-start h-screen">
+            {/* Centering container */}
+            <div className="flex flex-col items-center w-full px-8 lg:px-16 mt-20"> {/* Increased margin-top (mt-20) */}
+                {/* Main header */}
+                <h1 className="text-5xl text-center mb-4">PROJECT NAME</h1>
+                {/* Blue rectangle */}
+                <div className="relative w-11/12 h-96 rounded-3xl bg-blue-500 flex flex-col justify-end mt-2"> {/* Added flex and justify-end */}
+                    <h1 className="text-4xl text-black mt-4 ml-4">insideheader</h1> {/* Adjusted positioning */}
+                    {/* Additional text at the bottom of insideheader with reduced line break spacing */}
+                    <p className="text-black mt-2 ml-4" style={{ lineHeight: '1.2' }}>
+                        Additional text at the bottom
+                        <br />
+                        Line break here
+                        <br />
+                        Another line break
+                    </p>
                 </div>
             </div>
-
-            <div className="Info Content">
-                <div className="Info 1"></div>
-                <div className="Info 2"></div>
-                <div className="Info 3"></div>
-            </div>
-
         </div>
-    )
+    );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Define your custom colors here
+        primary: {
+          100: '#4d6b97',
+          200: '#0083ab',
+          300: '#009799',
+          300: '#00a564',
+          300: '#7caa15',
+        },
+        secondary: {
+          // Define more custom colors here
+        },
+      },
+    },
   },
   plugins: [],
 }
