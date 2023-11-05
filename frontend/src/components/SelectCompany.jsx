1be78d36b0af3fdfb3d3ccab741420c94c53155b
vinncceee <vbanks2k@gmail.com>
2023-11-05 07:56:13 -0600

    button

--- frontend/src/components/SelectCompany.jsx
+++ frontend/src/components/SelectCompany.jsx
@@ -6,22 +6,22 @@ import spanishData from '../constants/spanish.json';
 import hindiData from '../constants/hindi.json';
 
 const SelectCompany = () => {
-// set seffirent languages
-const languages = {
+  // set different languages
+  const languages = {
     english: englishData,
     spanish: spanishData,
     hindi: hindiData,
   };
 
   const [currentLanguage, setCurrentLanguage] = useState('english');
-  
+
   const changeLanguage = (language) => {
     setCurrentLanguage(language);
   };
 
   const [companies, setCompanies] = useState([
     { id: 1, checked: false, name: 'About' },
-    { id: 2, checked: false, name: 'checking account' },
+    { id: 2, checked: false, name: 'Checking account' },
     { id: 3, checked: false, name: 'Savings account' },
     { id: 4, checked: false, name: 'Brokerage' },
     { id: 5, checked: false, name: 'Online commissions' },
@@ -39,7 +39,7 @@ const languages = {
     'Wells Fargo',
     'Capital One',
   ];
-  
+
   const toggleCompanySelection = (id) => {
     const updatedCompanies = companies.map((company) =>
       company.id === id ? { ...company, checked: !company.checked } : company
@@ -53,12 +53,22 @@ const languages = {
 
   return (
     <div>
-      <Navbar changeLanguage={changeLanguage} language={languages[currentLanguage].lan} nav1={languages[currentLanguage].nav1} nav2={languages[currentLanguage].nav2} nav3={languages[currentLanguage].nav3} nav4={languages[currentLanguage].nav4}/> {/* Render the Navbar component here */}
+      <Navbar
+        changeLanguage={changeLanguage}
+        language={languages[currentLanguage].lan}
+        nav1={languages[currentLanguage].nav1}
+        nav2={languages[currentLanguage].nav2}
+        nav3={languages[currentLanguage].nav3}
+        nav4={languages[currentLanguage].nav4}
+      />{' '}
+      {/* Render the Navbar component here */}
       <div className="bg-primary-600 min-h-screen p-20">
         <div className="container mx-auto">
           <div className="mt-6">
-            <label htmlFor="financial-company-select" className="block mb-2 text-sm font-medium text-gray-900">
-            </label>
+            <label
+              htmlFor="financial-company-select"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            ></label>
             <select
               id="financial-company-select"
               value={selectedFinancialCompany}
@@ -75,7 +85,9 @@ const languages = {
               ))}
             </select>
           </div>
-          <h2 className="text-black text-4xl font-rubik mt-12 mb-4">Feel free to tell us what you're interested in or curious about.</h2>
+          <h2 className="text-black text-4xl font-rubik mt-12 mb-4">
+            Feel free to tell us what you're interested in or curious about.
+          </h2>
           <div className="bg-white p-5 rounded-md">
             <ul>
               {companies.map((company) => (
@@ -97,10 +109,22 @@ const languages = {
             {/* SVG image placed to the right */}
             <img src={img2} alt="SVG Image" className="w-56 mb-96" />
           </div>
+          {/* Start Chatting Button */}
+          <div className="text-center mt-8">
+            <button
+              onClick={() => {
+                // Add your logic to handle the "Start Chatting" button click
+                console.log('Start Chatting clicked');
+              }}
+              className="bg-blue-500 text-white rounded-full py-4 px-7 hover:bg-blue-600 transition duration-300 font-rubik w-52"
+            >
+              Start Chatting
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default SelectCompany;
